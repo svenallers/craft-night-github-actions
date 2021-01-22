565be06726c682fed03953cbdf0222c8884a67cb
Sven Allers <allers@silpion.de>
2021-01-22 17:12:37 +0100

    env

--- test.spec.js
+++ test.spec.js
@@ -1,5 +1,11 @@
 describe('example', function() {
     it('should execute a test', function() {
-        expect(true).toBe(false)
+        expect(true).toBe(true)
+    })
+    it('should find the secret', function() {
+        expect(process.env.TEST_SECRET).toBe("foo")
+    })
+    it('should find the secret (error)', function() {
+        expect(process.env.TEST_SECRET).toBe("foo2")
     })
 })
